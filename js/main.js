24ab0af2350a611d51161b2feae03e3361a37a1c
UriiDP <udp1212@gmail.com>
2025-03-19 05:48:08 -0600

    Aumento de velocidad por cada 10 peces atrapados y aparición de tiburones al pasar 50 peces atrapados y disminuyen a la mitad los peces atrapados hasta el momento

--- js/main.js
+++ js/main.js
@@ -6,14 +6,23 @@ const window_width = 1000;
 canvas.height = window_height;
 canvas.width = window_width;
 
-const maxCircles = 10;  // Permitir más de un pez, pero solo uno puede estar atrapado
+const music = new Audio('Media/Audio/chill_guy.mp3');
+music.loop = true;
+
+canvas.addEventListener("click", () => {
+    music.play().catch(error => {
+        console.log("Error al intentar reproducir la música:", error);
+    });
+});
+
+const maxCircles = 10;
 let fishGenerationInterval = 1500;
 let fishGenerationTimer;
 
-startFishGeneration();  // Iniciar la generación gradual de peces
+startFishGeneration();
 
 function startFishGeneration() {
-    fishGenerationTimer = setInterval(spawnCircles, fishGenerationInterval);  // Generar un pez cada 'fishGenerationInterval' ms
+    fishGenerationTimer = setInterval(spawnCircles, fishGenerationInterval);
 }
 
 document.addEventListener("mousemove", (event) => {
@@ -22,7 +31,7 @@ document.addEventListener("mousemove", (event) => {
 
     const minHeight = 190;
     const maxHeight = window_height - 20;
-    ceboPosY = Math.min(Math.max(mouseY, minHeight), maxHeight);  // Restringir la posición del cebo
+    ceboPosY = Math.min(Math.max(mouseY, minHeight), maxHeight);
 });
 
 const background = new Image();
@@ -33,7 +42,6 @@ function drawBackground() {
     ctx.drawImage(background, 0, 0, window_width, window_height);
 }
 
-// Estilos del canvas y página
 canvas.style.display = "block";
 canvas.style.margin = "auto";
 canvas.style.border = "5px solid #fff";
@@ -49,11 +57,13 @@ document.body.style.background = "linear-gradient(135deg, #1a1a2e, #16213e)";
 document.body.style.fontFamily = "Arial, sans-serif";
 document.body.appendChild(canvas);
 
-let totalCircles = 10;
 let removedCircles = 0;
+let baseFishSpeed = 1;
 let circles = [];
 let timeElapsed = 0; 
 let timer;
+let tiburones = []; // Almacenará todos los tiburones
+let tiburon; // Almacenará la instancia del tiburón
 
 const explosionSound = new Audio("Media/Audio/collision.mp3");
 
@@ -140,13 +150,13 @@ class Circle {
 }
 
 const barrilImage = new Image();
-barrilImage.src = "Media/Images/barril.png";  // Asegúrate de tener la imagen del barril
+barrilImage.src = "Media/Images/barril.png";
 
 class Barril {
     constructor(radius, speed, imageSrc) {
         this.posX = Math.random() > 0.5 ? -radius : window_width + radius;
         this.posY = Math.random() * (window_height / 2) + (window_height / 2); 
-        this.radius = radius * 2;  // El doble de tamaño
+        this.radius = 60;
         this.speed = speed;
         this.dx = this.posX < 0 ? speed : -speed;
         this.dy = 0.1;  
@@ -172,10 +182,44 @@ class Barril {
     }
 }
 
+const tiburonImage = new Image();
+tiburonImage.src = "Media/Images/tiburon.png"; // Asegúrate de tener la imagen del tiburón
+
+class Tiburon {
+    constructor(radius, speed, imageSrc) {
+        this.posX = Math.random() > 0.5 ? -radius : window_width + radius;
+        this.posY = Math.random() * (window_height / 2) + (window_height / 2);
+        this.radius = radius; // Usamos el radio pasado como parámetro
+        this.speed = speed;
+        this.dx = this.posX < 0 ? speed : -speed;
+        this.dy = 0.1;
+        this.image = new Image();
+        this.image.src = imageSrc;
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.posX - this.radius, this.posY - this.radius, this.radius * 2, this.radius * 2);
+    }
+
+    update() {
+        this.posX += this.dx;
+        this.posY += this.dy;
+        if (this.posY + this.radius > window_height || this.posY - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        if (this.posX + this.radius < 0 || this.posX - this.radius > window_width) {
+            this.posX = Math.random() > 0.5 ? -this.radius : window_width + this.radius;
+            this.posY = Math.random() * (window_height / 2) + (window_height / 2);
+        }
+    }
+}
+
+
 function spawnCircles() {
-    if (circles.length < maxCircles) {  // Permitir generar nuevos peces sin detener la generación
+    if (circles.length < maxCircles) {
         const radius = Math.random() * 20 + 30; 
-        const constantSpeed = 1;
+        const constantSpeed = baseFishSpeed;
         let newCircle = new Circle(radius, constantSpeed, "Media/Images/pez1.png");
         circles.push(newCircle);
     }
@@ -184,7 +228,7 @@ function spawnCircles() {
 let barriles = [];
 
 function spawnBarriles() {
-    if (barriles.length < maxCircles) {  // Permitir generar nuevos barriles sin detener la generación
+    if (barriles.length < maxCircles) {
         const radius = Math.random() * 20 + 30; 
         const constantSpeed = 1;
         let newBarril = new Barril(radius, constantSpeed, "Media/Images/barril.png");
@@ -208,15 +252,26 @@ canvas.addEventListener("click", (event) => {
     const rect = canvas.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
+    const atrapadoSonido = new Audio("Media/Audio/atrapar_pez.mp3");
 
-    if (mouseY <= 190) {  // Solo liberar el pez cuando se hace clic en la zona superior
+    if (mouseY <= 190) {
         for (let i = 0; i < circles.length; i++) {
             if (circles[i].attached) {
                 explosionSound.cloneNode().play();
                 removedCircles++;
+                if (removedCircles % 10 === 0) {
+                    baseFishSpeed += 0.5;
+                    circles.forEach(circle => {
+                        if (!circle.attached) {
+                            circle.speed = baseFishSpeed;
+                            circle.dx = circle.posX < window_width / 2 ? baseFishSpeed : -baseFishSpeed;
+                        }
+                    });
+                }
                 updateStats();
-                circles.splice(i, 1);  // Eliminar el pez atrapado de la lista
-                break;  // Salir del bucle después de liberar el pez
+                circles.splice(i, 1);
+                atrapadoSonido.play();
+                break;
             }
         }
     }
@@ -230,7 +285,6 @@ function checkCeboCollision() {
         let distance = Math.sqrt(dx * dx + dy * dy);
         
         if (distance < circle.radius + 20 && !circle.attached) {
-            // Solo atrapa el pez si no hay otro pez atrapado
             if (!circles.some(c => c.attached)) {
                 circle.attached = true;
             }
@@ -243,22 +297,42 @@ function checkBarrilCollision() {
         let barril = barriles[i];
         for (let j = 0; j < circles.length; j++) {
             let circle = circles[j];
-            if (circle.attached) {  // Solo verificar colisión con peces atrapados
+            if (circle.attached) {
                 let dx = barril.posX - circle.posX;
                 let dy = barril.posY - circle.posY;
                 let distance = Math.sqrt(dx * dx + dy * dy);
                 
                 if (distance < barril.radius + circle.radius) {
-                    // Reproducir el sonido de choque
                     const choqueSound = new Audio("Media/Audio/choque.mp3");
                     choqueSound.play();
-
-                    circles.splice(j, 1);  // Eliminar el pez atrapado
-                    break;  // Salir del bucle después de eliminar el pez
+                    circles.splice(j, 1);
+                    break;
                 }
             }
         }
     }
+}
+
+function checkTiburonCollision() {
+    tiburones.forEach((tiburon, index) => {
+        // Verificar colisión con el cebo
+        let dx = window_width / 2 - tiburon.posX;
+        let dy = ceboPosY - tiburon.posY;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < tiburon.radius + 20) {
+            // Restar la mitad de los peces atrapados
+            removedCircles = Math.floor(removedCircles / 2);
+            updateStats();
+
+            // Reproducir un sonido de choque (opcional)
+            const choqueSound = new Audio("Media/Audio/choque.mp3");
+            choqueSound.play();
+
+            // Eliminar el tiburón después de la colisión (opcional)
+            tiburones.splice(index, 1);
+        }
+    });
 }
 
 function updateBarriles() {
@@ -269,22 +343,52 @@ function updateBarriles() {
 }
 
 function updateCircles() {
-    requestAnimationFrame(updateCircles);  
-    drawBackground();  
+    requestAnimationFrame(updateCircles);
+    drawBackground();
 
+    // Dibuja el hilo de la caña de pescar
+    ctx.beginPath();
+    ctx.moveTo(window_width / 2, 90);
+    ctx.lineTo(window_width / 2, ceboPosY);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1.5;
+    ctx.stroke();
+
+    // Actualizar y dibujar peces
     circles.forEach((circle) => {
-        circle.update();  
-        circle.draw();  
+        circle.update();
+        circle.draw();
     });
 
+    // Actualizar y dibujar barriles
     barriles.forEach((barril) => {
-        barril.update();  
-        barril.draw();  
+        barril.update();
+        barril.draw();
     });
 
-    ctx.drawImage(cebo, window_width / 2 - 20, ceboPosY - 20, 40, 40);  
+    // Dibujar el cebo
+    ctx.drawImage(cebo, window_width / 2 - 20, ceboPosY - 20, 40, 40);
+
+    // Verificar colisiones
     checkCeboCollision();
     checkBarrilCollision();
+
+    // Generar tiburones después de 50 peces
+    if (removedCircles >= 50 && Math.random() < 0.01) { // Probabilidad de generar un tiburón
+        const radius = 100; // Tamaño más grande del tiburón
+        const speed = 2; // Velocidad del tiburón
+        const newTiburon = new Tiburon(radius, speed, "Media/Images/tiburon.png");
+        tiburones.push(newTiburon);
+    }
+
+    // Actualizar y dibujar tiburones
+    tiburones.forEach((tiburon) => {
+        tiburon.update();
+        tiburon.draw();
+    });
+
+    // Verificar colisiones con tiburones
+    checkTiburonCollision();
 }
 
 startTimer();
